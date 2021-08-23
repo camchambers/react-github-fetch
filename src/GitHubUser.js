25528f0cf5e0e06686ad38ef353c47da0ef5720d
Cam Chambers <cam_chambers@outlook.com>
2021-08-22 21:10:38 -0400

    Rendered additional GitHub user attributes

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,28 +1,33 @@
-import React, { 
-    useState, 
-    useEffect 
+import React, {
+    useState,
+    useEffect
 } from "react";
 
 // Fetch data from the GitHub API 
-function GitHubUser({ login }){
+function GitHubUser({ login }) {
     const [data, setData] = useState(null);
-    useEffect(()=> {
-      fetch(`https://api.github.com/users/${login}`)
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error);
-    },[login]);
-  
-    if (data){
-      return(
-        <div>
-          <h1>{data.login}</h1>
-          <img src={data.avatar_url} width={100} alt="User profile"/>
-          <p>{JSON.stringify(data)}</p>
-        </div>
-      )
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${login}`)
+            .then(res => res.json())
+            .then(setData)
+            .catch(console.error);
+    }, [login]);
+
+    if (data) {
+        return (
+            <div>
+                <h1>{data.login}</h1>
+                {data.email ? <p>{data.email}</p> : ""}
+                <img src={data.avatar_url} width={100} alt="GitHub profile picture" />
+                {data.name ? <p>{data.name}</p> : ""}
+                {data.html_url ? <p>{data.html_url}</p> : ""}
+                {data.company ? <p>{data.company}</p> : ""}
+                {data.bio ? <p>{data.bio}</p> : ""}
+                <p>Public Repos: {data.public_repos}</p>
+            </div>
+        )
     }
     return null;
-  }
+}
 
 export default GitHubUser;
